bc0289a48e7e36898ff5f42258f659ebb4a709db
clementayz <clementayz@gmail.com>
2022-03-27 15:31:34 +0800

    add project components

---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <section id='projects'>
+            <h5>What I have done</h5>
+            <h2>Projects</h2>
+            <div className="container">
+
+            </div>
+        </section>
+    )
+}
+
+export default Projects
