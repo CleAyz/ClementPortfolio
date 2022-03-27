19be04f203e680d8450707329d5df47c7fe8dbc6
clementayz <clementayz@gmail.com>
2022-03-27 15:32:36 +0800

    update layout of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Projects from "./components/projects/Projects";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Nav /> */}
+      <Nav />
       <About />
+      <Experience />
+      {/* <Projects /> */}
+      <Contact />
+      <Footer />
     </>
   );
 }
