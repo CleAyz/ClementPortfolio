696ca1b8daf36d7475d4b12afd21af2f522ff1bf
clementayz <clementayz@gmail.com>
2022-04-20 23:46:09 +0800

    add social github

--- src/components/header/HeaderSocials.tsx
+++ src/components/header/HeaderSocials.tsx
@@ -6,7 +6,7 @@ const HeaderSocials = () => {
     return (
         <div className='socials'>
             <a href="https://www.linkedin.com/in/clementayz4341595a4849/" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a>
-            {/* <a href="" target="_blank" rel="noopener noreferrer"><FaGithub /></a> */}
+            <a href="https://github.com/CleAyz/" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
         </div>
     )
 }
